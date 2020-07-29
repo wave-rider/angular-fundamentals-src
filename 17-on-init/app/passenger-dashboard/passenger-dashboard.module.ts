bf1dc810c7eaf032363825a112d0cad168b9c682
wave-rider <wave-rider@users.noreply.github.com>
2020-07-30 05:32:32 +1200

    added 2 components

--- 17-on-init/app/passenger-dashboard/passenger-dashboard.module.ts
+++ 17-on-init/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
 @NgModule({
   declarations: [
-    PassengerDashboardComponent
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
   ],
   imports: [
     CommonModule
